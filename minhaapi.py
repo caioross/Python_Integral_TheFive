f4bd6056cba975c0fbdfc3fb0c7c4313de8f4ad5
Bruno Bindi <117173078+Bruno-Bindi@users.noreply.github.com>
2022-11-03 17:29:23 -0300

    minhaapi nao esta executando
    minhaai nao executa, erro 404!

--- minhaapi.py
+++ minhaapi.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask (__name__)
 
@@ -7,12 +7,43 @@ app = Flask (__name__)
 def pagina_inicial():
   return 'Melhor turma do mundo'
 
+@app.route('/index/')
+def index():
+  return 'aqui'
+
 @app.route('/total/')
 def total():
   tabela = pd.read_csv('base.csv')
-  tabela_vendas = tabela['Vendas'].sum()
+  total_vendas = tabela['Vendas'].sum()
   resposta = {'Total Vendas:': total_vendas}
   return jsonify(resposta)
 
+
+  
+@app.route('/media/')
+def media():
+  tabela = pd.read_csv('base.csv')
+  media_vendas = tabela['Vendas'].mean()
+  resposta = {'media Vendas:': media_vendas}
+  return jsonify(resposta)
+
+
+
+@app.route('/composto/')
+def composto():
+  tabela = pd.read_csv('base.csv')
+  media_vendas = tabela['Vendas'].mean()
+  total_vendas = tabela['Vendas'].sum()
+  resposta = {'media Vendas:': media_vendas, 'Total Vendas:':total_vendas}
+  return jsonify(resposta)
+
+
+  session = 'logado'
+  @app.route('/admin/')
+  def admin_index():
+    if('logado' not in session):
+      return redirect(url_for('index'))
+    return render_template('index.html')
+    
 if __name__ == '__main__':
   app.run('0.0.0.0')
