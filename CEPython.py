ac14f4729cc1f63b4e79add4cc1aaff41005565b
EnzoZR0 <117111153+EnzoZR0@users.noreply.github.com>
2022-11-03 16:38:31 -0300

    CEPython.py
    achar o cep

---
+++ CEPython.py
@@ -0,0 +1,38 @@
+import requests
+
+def main():
+	print('CEPython')
+
+	cep = input('Digite o CEP para a consulta: ')
+	
+	while len(cep) != 8:
+		print('Quantidade de dígitos inválida!')
+		cep = input('Digite o CEP para a consulta: ')
+
+	request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep))
+	endereco = request.json()
+
+	if 'erro' not in endereco:
+		print('==> CEP ENCONTRADO <==')
+		print('CEP: {}'.format(endereco['cep']))
+		print('Cidade: {}'.format(endereco['localidade']))
+		print(f"Estado: {endereco['uf']}")
+		
+	else:
+		print('{}: CEP inválido.'.format(cep))
+
+	print('---------------------------------')
+ 
+	def validacao():
+		opcao = input('Deseja realizar outra consulta ? \n 1. Sim \n 2. Sair \n ')
+		if opcao == "1" or opcao == "2":
+			if int(opcao) == 1:
+				main()
+			else:
+				print('Até mais!!!')
+		else:
+			validacao()
+	validacao()
+
+if __name__ == '__main__':
+	main()
