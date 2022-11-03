12acc776ed539ca89c9dc852e34be9f5841579fb
EnzoZR0 <117111153+EnzoZR0@users.noreply.github.com>
2022-11-03 17:21:09 -0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+  return 'Melhor site daqui'
+
+@app.route('/total/')
+def total():
+  tabela = pd.read_csv('base.csv')
+  total_vendas = tabela ['Vendas'].sum()
+  resposta = {'Total Vendas:': total_vendas}
+  return jsonify (resposta)
+
+@app.route('/media/')
+def media():
+  tabela = pd.read_csv('base.csv')
+  media_vendas = tabela ['Vendas'].mean()
+  resposta = {'Media Vendas:': media_vendas}
+  return jsonify (resposta)
+
+@app.route('/composto/')
+def composto():
+  tabela = pd.read_csv('base.csv')
+  media_vendas = tabela ['Vendas'].mean()
+  total_vendas = tabela ['Vendas'].sum()
+  resposta = {'Media Vendas:': media_vendas, 'Total Vendas:': total_vendas}
+  return jsonify(resposta)
+
+
+session = ""
+@app.route('/admin/')
+def admin_index():
+  if ('logado' not in session):
+    return redirect(url_for ('index'))
+  return render_template('/templates/index.html')
+    
+
+if __name__ == "__main__":
+  app.run('0.0.0.0')
+
+
+  
+  
