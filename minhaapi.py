152b7baef4115a07054d5fb3b574e63c9c44df40
Caio Ross <49957185+caioross@users.noreply.github.com>
2022-11-03 14:20:06 -0300

    Upload da api pessoal
    primeira versão ainda com erro 500

---
+++ minhaapi.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+  return 'Melhor turma do mundo'
+
+@app.route('/total/')
+def total():
+  tabela = pd.read_csv('base.csv')
+  total_vendas = tabela['Vendas'].sum()
+  resposta = {'Total Vendas:': total_vendas}
+  return jsonify(resposta)
+
+if __name__ == "__main__":
+  app.run('0.0.0.0')
